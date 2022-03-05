7a409db96cd29473a4929bdaba8d9d2ea96a2e9e
Marco Lazzarin <48728634+mlazzarin@users.noreply.github.com>
2022-03-05 18:21:16 +0100

    Add PortfolioOptimizer model

---
+++ finance/main.py
@@ -0,0 +1,63 @@
+"""
+Author: Marco Lazzarin
+Description: Main file for "Deep learning for portfolio optimization"
+"""
+
+# Set env variables for reproducibility
+# pylint: disable=wrong-import-position
+import os
+os.environ['PYTHONHASHSEED'] = str(2022)
+os.environ['CUDA_VISIBLE_DEVICES'] = ""
+
+# Libraries
+import random
+
+import numpy as np
+import tensorflow as tf
+
+from data import load_data
+from model import PortfolioOptimizer
+
+# https://keras.io/getting_started/faq/
+# The below is necessary for starting Numpy generated random numbers
+# in a well-defined initial state.
+np.random.seed(2022)
+
+# The below is necessary for starting core Python generated random numbers
+# in a well-defined state.
+random.seed(2022)
+
+# The below set_seed() will make random number generation
+# in the TensorFlow backend have a well-defined initial state.
+# For further details, see:
+# https://www.tensorflow.org/api_docs/python/tf/random/set_seed
+tf.random.set_seed(2022)
+
+
+def main():
+    """Main function."""
+
+    # ----- LOAD AND PREPROCESS DATA -----
+
+    # Retrive data the historical series and concatenate them in a single
+    # NumPy array of shape (1, timesteps, 2 * n_assets)
+    data = np.concatenate((load_data("data/VTI.csv", ["Close"]),
+                           load_data("data/AGG.csv", ["Close"]),
+                           load_data("data/DBC.csv", ["Close"]),
+                           load_data("data/VIX.csv", ["Close"])), axis=1)
+    data = np.squeeze(data)
+    data = data.reshape(1, *data.shape) # reshape the data to expose the batch size (one)
+
+    # Extract the returns (third index, odd numbers) and double-check the correctness
+    returns = data[:, :, 1::2]
+    test_returns = data[:, 1:, 0::2] / data[:, :-1, 0::2] - 1.0
+    np.testing.assert_equal(returns[:, 1:, :], test_returns)
+
+    # ----- BUILD AND TRAIN MODEL -----
+
+    model = PortfolioOptimizer(data)
+    model.build()
+    model.fit()
+
+if __name__ == "__main__":
+    main()
---
+++ finance/model.py
@@ -0,0 +1,49 @@
+"""
+Author: Marco Lazzarin
+Description: Deep learning model from "Deep learning for portfolio optimization"
+"""
+
+# Libraries
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input, LSTM, TimeDistributed
+
+from metrics import sharpe, portfolio_returns
+
+
+class PortfolioOptimizer():
+    """Deep learning model for portfolio optimization"""
+    # pylint: disable=too-many-instance-attributes
+
+    def __init__(self, data):
+        """Initialize data members"""
+
+        # Train-valid-test split
+        sequences_len  = data.shape[1]
+        self.train_set = data[:, :sequences_len//2, :]
+        self.valid_set = data[:, sequences_len//2:3*sequences_len//4, :]
+        self.test_set  = data[: ,3*sequences_len//4:, :]
+
+        # Compute returns
+        self.train_returns = self.train_set[:, :, 1::2]
+        self.valid_returns = self.valid_set[:, :, 1::2]
+        self.test_returns  = self.test_set[:, :, 1::2]
+
+        # Various
+        self.n_features = data.shape[2]
+        self.history    = None
+        self.model      = None
+
+    def build(self):
+        """Build the deep learning model."""
+        self.model = Sequential()
+        self.model.add(Input(shape=(None, self.n_features)))
+        self.model.add(LSTM(64, return_sequences=True))
+        self.model.add(TimeDistributed(Dense(4, activation="softmax")))
+        self.model.compile(loss=sharpe, optimizer="adam", metrics=[portfolio_returns])
+        self.model.summary()
+
+    def fit(self, epochs=100):
+        """Fit the deep learning model."""
+        self.history = self.model.fit(self.train_set, self.train_returns,
+                                      validation_data=(self.valid_set, self.valid_returns),
+                                      epochs=epochs)
